67525230d5c518aa346a61122bb53aff185fbbd6
Ming Han Yap <mhyap999@gmail.com>
2016-12-03 10:36:13 +0800

    #135037631

---
+++ OrderedList/OrderedList/FrmPayment.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Text
+Public Class FrmPayment
+    Public Property AutoSizeColumnsMode As DataGridViewAutoSizeColumnsMode
+    Friend totalPrice As Decimal
+    Private db As BurzlyDataContext
+    Friend tableNo As String
+    'Private db As BurzlyDataContext
+    Private Sub FrmPayment_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        totalPrice = 0
+        'frmProductMenu.tableNumber = tableNo
+        Dim db As New BurzlyDataContext
+
+        txtTable.Text = tableNo
+ 
+        txtDate.Text = CStr(Date.Now.Date())
+        txtTime.Text = DateTime.Now.ToShortTimeString()
+        txtTime.Enabled = False
+        txtDate.Enabled = False
+        txtPaymentID.Enabled = False
+        txtTable.Enabled = False
+
+    End Sub
+
+
+End Class
